3ae42ca3f8c479b00e6666427b35b9472ea5396a
Eugen Hildt <eugen.hildt@posteo.de>
2023-01-31 19:58:38 +0100

    wip on variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+
+const test = "test"
+// test = "fdfdf" this is gonna break
+
+const a = [2, 3, 4, 5, 6, 7]
+let b = { name: "jan" }
+let c = [{ name: "jan" }, [1, 2, 3, 4], () => { }]
+
+let d = 3
+
+console.log(d + 5)
